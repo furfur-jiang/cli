dcab61fc3323b80967c230aad1cd9e6ca0374375
江茂琼 <jiangmaoqiong@lizhi.fm>
2021-11-25 10:40:16 +0800

    fix

--- cli-fur/cli.js
+++ cli-fur/cli.js
@@ -7,21 +7,22 @@ const ejs = require('ejs')
 inquirer
   .prompt([{ type: 'input', name: 'name', message: '项目名' }])
   .then((ans) => {
-    console.log(ans)
     const tmpDir = path.join(__dirname, 'templates') //模板路径
     const destDir = process.cwd() //需要写入的路径
 
     fs.readdir(tmpDir, (err, files) => {
       if (err) throw err
       files.forEach((file) => {
-        ejs.renderFile(
-          path.join(tmpDir, file), //文件路径+名
-          ans, //替换内容
-          (err, res) => {
-            if (err) throw err
+        ejs
+          .renderFile(
+            // renderFile（模版文件地址，传入渲染数据）
+            path.join(tmpDir, file), //文件路径+名
+            ans, //替换内容
+          )
+          .then((res) => {
+            // 生成 ejs 处理后的模版文件
             fs.writeFileSync(path.join(destDir, file), res) //写入文件
-          },
-        )
+          })
       })
     })
   })
